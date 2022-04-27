92c94cad990104610b998003361490d533ab64ce
MarwenTlili <marwentlili@gmail.com>
2022-04-27 01:43:58 +0100

    remove tmp

--- test.rb
+++
@@ -1,7 +0,0 @@
-result = ""
-message = "GRAVE: Servlet.service() for servlet [DispatcherServlet] in context with path [/ma-gpro-gpao-rest] threw exception"
-result = message.scan(/\[[^]]+\]/)
-print result[0]
-print "\n"
-
-
